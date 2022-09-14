09e1f96023eab30b13c037e694da0a64e36d4c56
hssh18121 <hssh18121@gmail.com>
2022-09-14 22:44:53 +0700

    Them admin va phan quyen cho user va admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "Account and all associated articles successfully deleted"
     redirect_to articles_path, status: :see_other
 
@@ -56,8 +56,8 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
-      flash[:danger] = "You could only edit your own account"
+    if current_user != @user && !current_user.admin?
+      flash[:danger] = "You could only edit or delete your own account"
       redirect_to @user
     end
   end
